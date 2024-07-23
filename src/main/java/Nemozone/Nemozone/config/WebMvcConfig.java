87fd2c630b07cba7ab30ca3b34694f6c693a6c53
Yun-YeoJun <yyj8771@naver.com>
2024-07-24 04:13:48 +0900

    cors 수정

--- src/main/java/Nemozone/Nemozone/config/WebMvcConfig.java
+++ src/main/java/Nemozone/Nemozone/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package Nemozone.Nemozone.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
